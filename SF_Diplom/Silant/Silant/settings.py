30c6f8806f3ce18c43beb8735677f5ea76ef0d0d
ram-FELT <103208662+ram-FELT@users.noreply.github.com>
2023-04-06 16:26:50 +0400

    Update settings.py

--- SF_Diplom/Silant/Silant/settings.py
+++ SF_Diplom/Silant/Silant/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     "api.apps.ApiConfig",
     "rest_framework",
     "frontend.apps.FrontendConfig",
-    "ckeditor",
     "rest_framework.authtoken",
     "dj_rest_auth",
     "corsheaders",
